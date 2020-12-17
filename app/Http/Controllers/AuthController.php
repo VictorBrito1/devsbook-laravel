ec3bc30772b28ac3c2ade958ef6b314731752375
Victor Brito <victor.brito@izap.com.br>
2020-12-17 17:31:37 -0300

    AuthController - login - logout - refresh

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -14,18 +15,60 @@ class AuthController extends Controller
         $this->middleware('auth:api', ['except' => ['login', 'unauthorized']]);
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function login(Request $request)
     {
+        $email = $request->input('email');
+        $password = $request->input('password');
 
+        $validator = Validator::make(['email' => $email, 'password' => $password], [
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string', 'min:6'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $token = auth()->attempt([
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        if (!$token) {
+            return response()->json(['error' => 'Wrong email or password.'], 401);
+        }
+
+        return response()->json(['token', $token]);
     }
 
-    public function logout(Request $request)
-    {
 
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+        return response()->json(['success' => true]);
     }
 
-    public function refresh(Request $request)
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
     {
+        $token = auth()->refresh();
+        return response()->json(['token' => $token]);
+    }
 
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function unauthorized()
+    {
+        return response()->json(['error' => 'Unauthorized'], 401);
     }
 }
