e4bf7faacd82d9bfcf62845a6adb2c9e7960af0a
Victor Brito <victor.brito@izap.com.br>
2021-01-12 22:33:49 -0300

    Search users

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
@@ -16,5 +17,31 @@ class SearchController extends Controller
         $this->middleware('auth:api');
 
         $this->currentUser = auth()->user();
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function search(Request $request)
+    {
+        $text = $request->get('text', null);
+
+        if ($text) {
+            $data = [];
+            $users = User::where('name', 'like', "%$text%")->get();
+
+            foreach ($users as $user) {
+                $data['users'][] = [
+                    'id' => $user['id'],
+                    'name' => $user['name'],
+                    'avatar' => url("media/avatars/{$user['avatar']}"),
+                ];
+            }
+
+            return response()->json($data);
+        }
+
+        return response()->json(['errors' => ['text' => ['The text field is required.']]], 400);
     }
 }
